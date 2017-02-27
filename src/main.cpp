6a13bab40485c5cc4359e612d848b43fe65fc4e6
Gennady Zavodovskiy <ga.zavodovskiy@jet.msk.su>
2017-02-27 19:18:29 +0300

    adc

--- src/main.cpp
+++ src/main.cpp
@@ -30,15 +30,14 @@ int main()
 //  init_TIM7(); // Green LED interrupt blink
   init_AD9833();
   init_LED();
-
   LCD_Init();
-
-  u16 clrs[]={BLUE,RED,GREEN,YELLOW,LGRAY,MAGENTA,BROWN,BLACK};
-  int i=0;
 
 //  FLASH->ACR &= (~FLASH_ACR_PRFTEN);
   FLASH_PrefetchBufferCmd(DISABLE);
   init_ADC();
+
+  u16 clrs[]={BLUE,RED,GREEN,YELLOW,LGRAY,MAGENTA,BROWN,BLACK};
+  int i=0;
 
   while(1)
   {
@@ -116,24 +115,24 @@ void init_TIM4()
   uint16_t CCR2_Val = 1333;
   uint16_t PrescalerValue;
 
-  /* TIM4 clock enable */
+  // TIM4 clock enable
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM4, ENABLE);
 
-  /* Compute the prescaler value */
-  PrescalerValue = (uint16_t) (((SystemCoreClock /2) / 10000) - 1); // 8399
+  // Compute the prescaler value, TIM4CLK = APB1*2 = 84MHz
+  PrescalerValue = (uint16_t) (((SystemCoreClock /2) / 10000) - 1); // 8399;  84M > 1Hz | 8,4M > 10Hz | 840K > 100Hz .. 8400 > 10KHz
 
-  /* Time base configuration */
-  TIM_TimeBaseStructure.TIM_Period = 9999; // TODO why 10 000? Must be 100 000
+  // Time base configuration
+  TIM_TimeBaseStructure.TIM_Period = 9999; // 10KHz input freq;  10K > 1Hz
   TIM_TimeBaseStructure.TIM_Prescaler = PrescalerValue;
   TIM_TimeBaseStructure.TIM_ClockDivision = TIM_CKD_DIV1;
   TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;
   TIM_TimeBaseStructure.TIM_RepetitionCounter = 0;
   TIM_TimeBaseInit(TIM4, &TIM_TimeBaseStructure);
 
-  /* TIM4 enable counter */
+  // TIM4 enable counter
   TIM_Cmd(TIM4, ENABLE);
 
-  /* PWM1 Mode configuration: Channel2 */
+  // PWM1 Mode configuration: Channel2
   TIM_OCInitStructure.TIM_OCMode = TIM_OCMode_PWM1;
   TIM_OCInitStructure.TIM_OutputState = TIM_OutputState_Enable;
   TIM_OCInitStructure.TIM_Pulse = CCR2_Val;
@@ -146,28 +145,28 @@ void init_TIM4()
 
 void init_TIM7()
 {
-  //------------------Инициализация TIM7------------------
+  // ------------------Инициализация TIM7------------------
   RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM7,ENABLE);
-  //По умолчанию частота шины 24 МГц при использовании кварца 8 МГц ????
-  TIM7->PSC = 24000 - 1;      //Настройка делителя на 1000 "тиков" в секунду
-  TIM7->ARR = 500;            //Отработка прерывания раз в секунду
-  TIM7->DIER |= TIM_DIER_UIE; //Разрешения прерывание от таймера
-  TIM7->CR1 |= TIM_CR1_CEN;   //Запуск таймера
-  NVIC_EnableIRQ(TIM7_IRQn);  //Разрешение TIM7_IRQn прерывания
-  //------------------------------------------------------
+  // По умолчанию частота шины 82 МГц
+  TIM7->PSC = 24000 - 1;      // Настройка делителя на 4000 "тиков" в секунду
+  TIM7->ARR = 500;            // Отработка прерывания 8 раз в секунду
+  TIM7->DIER |= TIM_DIER_UIE; // Разрешения прерывание от таймера
+  TIM7->CR1 |= TIM_CR1_CEN;   // Запуск таймера
+  NVIC_EnableIRQ(TIM7_IRQn);  // Разрешение TIM7_IRQn прерывания
+  // ------------------------------------------------------
 }
 
 
 void init_LED() // init GPIO PD12 for Green LED
 {
-  //------------------Инициализация портов светодиодов------------------
-  GPIO_InitTypeDef GPIO_InitStructure;                //Структура содержащая настройки порта
-  RCC_AHB1PeriphClockCmd(LED_CLOCKPORT, ENABLE);      //Включаем тактирование порта D
-  GPIO_InitStructure.GPIO_Pin = LED_PIN;              //Выбираем нужные выводы | GPIO_Pin_13 | GPIO_Pin_14| GPIO_Pin_15
-  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;       //Включаем режим выхода
+  // ------------------Инициализация портов светодиодов------------------
+  GPIO_InitTypeDef GPIO_InitStructure;                // Структура содержащая настройки порта
+  RCC_AHB1PeriphClockCmd(LED_CLOCKPORT, ENABLE);      // Включаем тактирование порта D
+  GPIO_InitStructure.GPIO_Pin = LED_PIN;              // Выбираем нужные выводы | GPIO_Pin_13 | GPIO_Pin_14| GPIO_Pin_15
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;       // Включаем режим выхода
   GPIO_InitStructure.GPIO_Speed = GPIO_Speed_100MHz;
   GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
   GPIO_InitStructure.GPIO_PuPd  = GPIO_PuPd_DOWN;
-  GPIO_Init(LED_PORT, &GPIO_InitStructure);           //вызов функции инициализации
-  //--------------------------------------------------------------------
+  GPIO_Init(LED_PORT, &GPIO_InitStructure);           // вызов функции инициализации
+  // --------------------------------------------------------------------
 }
